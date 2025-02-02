60147b7b8628357ca889ec81c0b364f8c816b992
JamirKhan7 <jamirkhan007@gmail.com>
2025-02-02 08:12:04 +0530

    [Picture]: uploader

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,14 @@
 class Image < ApplicationRecord
   belongs_to :user
   mount_uploader :picture, PictureUploader
+
+  validate :image_size
+
+  private
+
+  def image_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
 end
